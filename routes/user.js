2aa7c1b4e46be79ac3bb45d1bcbca1773495d729
PrasunShrivastav <shrivastavprasun36@gmail.com>
2025-06-24 20:18:30 +0530

    added password hashing

--- routes/user.js
+++ routes/user.js
@@ -1,6 +1,18 @@
 const { Router } = require("express");
 const userRouter = Router();
-userRouter.post("/signup", (req, res) => {});
+const bcrypt = require("bcrypt");
+userRouter.post("/signup", async (req, res) => {
+  const email = req.body.email;
+  const password = req.body.password;
+  const name = req.body.name;
+  try {
+    const hashedPassword = await bcrypt.hash(password, 10);
+  } catch (e) {
+    res.status(404).json({
+      error: e,
+    });
+  }
+});
 userRouter.post("/signin", (req, res) => {});
 userRouter.get("/course", (req, res) => {});
 module.exports = {
